3a7e6c8b9026fc575ece4ceb4eaaa53d8bf9d3a3
LanternControl <brett4president@hotmail.com>
2017-09-13 01:36:07 -0400

    Couldn't figure out three of them

--- starter_files/classroom.rb
+++ starter_files/classroom.rb
@@ -2,18 +2,35 @@
 # for that student and assignment. Note that Ruby counts arrays from 0, but
 # we are referring to them as 1-10.
 def assignment_score(grade_hash, student, assignment_num)
+  new_array = []
+  new_array = grade_hash[student]
+  [assignment_num-1]
+end
+  score
 end
 
 # Given a grade_hash and assignment number, return all scores for that
 # assignment. Note that Ruby counts arrays from 0, but we are referring to
 # them as 1-10.
 def assignment_scores(grade_hash, assignment_num)
+  new_array = []
+  grade_hash.map do |name, assignment|
+    new_array << assignment[assignment_num-1]
+  end
+   new_array
 end
 
 # Given a grade_hash and assignment number, return the average score for that
 # assignment. Note that Ruby counts arrays from 0, but we are referring to
 # them as 1-10.
 def assignment_average_score(grade_hash, assignment_num)
+  average = 0
+  grade_hash.map do |name, assignment|
+
+    #why do I have to put average after =
+    average = average + assignment[assignment_num-1]
+  end
+  average = average / grade_hash.length
 end
 
 # Return a hash of students and their average score.
@@ -29,6 +46,28 @@ end
 # 60-69 => D
 # < 60 => F
 def letter_grade(score)
+  # if (0..59).include?(score)
+  #    puts "F"
+  # elsif (60..69).include?(score)
+  #    puts "D"
+  # elsif (70..79).include?(score)
+  #    puts "C"
+  # elsif (80..89).include?(score)
+  #    puts "B"
+  # else
+  #    puts "A"
+  # end
+  if score >= 90
+    return "A"
+  elsif score >= 80
+    return "B"
+  elsif score >= 70
+    return "C"
+  elsif score >= 60
+    return "D"
+  else
+    return "F"
+  end
 end
 
 # Return a hash of students and their final letter grade, as determined
@@ -38,6 +77,10 @@ end
 
 # Return the average for the entire class.
 def class_average(grade_hash)
+  average = grade_hash.map do |x, y|
+    y.reduce(0, :+).to_i / y.size
+  end
+  average.reduce(0, :+).to_i / average.size
 end
 
 # Return an array of the top `number_of_students` students.
